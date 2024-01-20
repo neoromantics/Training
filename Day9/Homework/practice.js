a1d408ce7ba803d553909df30fb2370bbbc50dff
neoromantics <newromantics2001@gmail.com>
2024-01-19 17:07:12 -0800

    improve

--- Day9/Homework/practice.js
+++ Day9/Homework/practice.js
@@ -84,13 +84,13 @@ const packages = [
 //a package name and a rating(number between 0 and 10),
 //and returns all shows available with that package,
 //with that rating or higher
-const showsWithRating = (package, _rating) => {
-  const networks = package.networks;
-  const availableShows = shows.filter((show) =>
-    networks.includes(show.network)
+function filter_shows(packageName, rating) {
+  const pack = packages.find((pack) => pack.name === packageName);
+  const { networks } = pack;
+  return shows.filter(
+    (show) => show.imdbRating >= rating && networks.includes(show.network)
   );
-  return availableShows.filter((show) => show.rating > _rating);
-};
+}
 
 //given an array and a chunk size, divide the
 //array into many sub-arrays where each sub-array
@@ -99,7 +99,6 @@ const showsWithRating = (package, _rating) => {
 //should return [[1,2,3,4,5],[6,7,8,9,10],[11,12,13]]
 
 const chunkArray = (array, num) => {
-  const remainder = array.length % num;
   const results = [];
   for (let i = 0; i < array.length; i = i + num) {
     const result = array.slice(i, i + num);
