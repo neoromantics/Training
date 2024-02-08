cda22562168e1129fc4eac96582091acad3cf1e4
LIU <tliu5@uw.edu>
2024-02-08 12:28:11 -0800

    fix

--- Day22/Album Viewer/src/routes.ts
+++
@@ -1,6 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import HomePage from "./HomePage";
-
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
-
-export default router;
---
+++ Day22/Album Viewer/src/routes.tsx
@@ -0,0 +1,4 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./HomePage";
+
+export default createBrowserRouter([{ path: "/", element: <HomePage /> }]);
