2b128fa161275c7e0a8a76f04d97d2a647ee63bf
LIU <tliu5@uw.edu>
2024-02-07 20:56:58 -0800

    good

---
+++ Day20/Tic Tac Toe/src/Components/Square.tsx
@@ -0,0 +1,15 @@
+import "../App.css";
+interface Props {
+  val: string;
+  chooseSquare: () => void;
+}
+
+function Square({ val, chooseSquare }: Props) {
+  return (
+    <div className="square" onClick={chooseSquare}>
+      {val}
+    </div>
+  );
+}
+
+export default Square;
