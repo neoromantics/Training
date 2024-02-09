b38f663e4b5aa54511ff2d385a6edb17ec8ce1df
LIU <tliu5@uw.edu>
2024-02-08 17:54:53 -0800

    improve

--- Day22/Album Viewer/src/ListPage.tsx
+++ Day22/Album Viewer/src/ListPage.tsx
@@ -1,21 +1,18 @@
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
-export interface Todo {
-  fakeProperty: string;
-  userId: number;
+export interface Album {
   id: number;
   title: string;
-  completed: boolean;
 }
 
 const ListPage = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [Albums, setAlbums] = useState<Album[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchTodos = async () => {
+    const fetchAlbums = async () => {
       try {
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/albums"
@@ -23,17 +20,17 @@ const ListPage = () => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
-        const data: Todo[] = await response.json();
-        setTodos(data);
+        const data: Album[] = await response.json();
+        setAlbums(data);
       } catch (error) {
-        setError("Failed to fetch todos");
+        setError("Failed to fetch Albums");
         console.error("There was a problem with the fetch operation:", error);
       } finally {
         setLoading(false);
       }
     };
 
-    fetchTodos();
+    fetchAlbums();
   }, []);
 
   return (
@@ -41,11 +38,11 @@ const ListPage = () => {
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       <ul>
-        {todos.slice(0, 10).map((todo) => (
-          <li key={todo.id}>
+        {Albums.slice(0, 10).map((album) => (
+          <li key={album.id}>
             <div>
-              <NavLink to={`/photo/${todo.id}`}>
-                {todo.id}: {todo.title}
+              <NavLink to={`/photo/${album.id}`}>
+                {album.id}: {album.title}
               </NavLink>
             </div>
           </li>
