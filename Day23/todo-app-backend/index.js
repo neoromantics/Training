c271748de72be63fed799c28351b50408eca8ef6
LIU <tliu5@uw.edu>
2024-02-15 11:22:18 -0800

    improve

--- Day23/todo-app-backend/index.js
+++ Day23/todo-app-backend/index.js
@@ -10,10 +10,10 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("❌❌❌Connected to MongoDB...❗️❗️❗️"))
+  .then(() => console.log("Connected to MongoDB..."))
   .catch((err) =>
     console.error(
-      "Could not connect to MongoDB..."
+      "❌❌❌Could not connect to MongoDB...❗️❗️❗️"
       // , err
     )
   );
